ddca8617125ac32c4757cdeae48374cae3b588d0
Takara Truong <truont2@gmail.com>
2022-02-04 22:53:20 -0800

    routes functional

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,6 +34,7 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new tag
+  // if the tag created were connected to a product then we need to update the product to unlude this tags id
   try {
     const newTag = await Tag.create(req.body);
     res.status(200).json(newTag);
